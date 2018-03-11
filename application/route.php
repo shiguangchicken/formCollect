09f5586550ced82dc82ebaa8c5e144f46a3bb35e
shiguangchicken <31305323+shiguangchicken@users.noreply.github.com>
2018-03-11 19:38:00 +0800

    1

---
+++ application/route.php
@@ -0,0 +1,25 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2016 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+/*return [
+    '__pattern__' => [
+        'name' => '\w+',
+    ],
+    '[hello]'     => [
+        ':id'   => ['index/hello', ['method' => 'get'], ['id' => '\d+']],
+        ':name' => ['index/hello', ['method' => 'post']],
+    ],*/
+    return [/*'student'=>'index/Index/add_student_info',
+        'admin_in'=>'index/Index/index',
+        'admin_in'=>'index/Index/',
+        'admin_in'=>'index/Index/index',
+        ''=>''*/
+];
